5e9084e1c6eeb4177ec06a19886441e260674720
NicolssKTH <Nicolss@kth.se>
2017-05-04 15:13:31 +0200

    Update InspectionRegistry.java

--- src/se/kth/iv1350/inspectcar/integration/InspectionRegistry.java
+++ src/se/kth/iv1350/inspectcar/integration/InspectionRegistry.java
@@ -1,8 +1,5 @@
 package se.kth.iv1350.inspectcar.integration;
 
-/**
- * Created by Röglas on 2017-05-03.
- */
 
 /*Inspection item*/
 public class InspectionRegistry {
@@ -36,22 +33,5 @@ public class InspectionRegistry {
     }
 }
 
-
-
-
-
-	/*
-	public String InspectionList(Inspection inspection) {
-		return null;
-	}
-
-	public Inspections findInspection(String regNo) {
-		return null;
-	}
-
-	public Result InspectionResult(Inspection inspection) {
-		return null;
-	}
-	*/
 
 
